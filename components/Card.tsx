01e93b4f3ba19a3541e71537189f60a06d16cc7c
SolarMax97 <maximilian.muehlhause@web.de>
2022-02-01 12:48:58 +0100

    dsdf. serv

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,18 +1,24 @@
-const Card = ({title,text,classes,img_src}:any) => {
+const Card = ({ title, text, classes, img_src, button }: any) => {
 
     return (
 
 
-        <div className={`border w-72 h-96 ${classes} rounded-xl border-gray-600 shadow-xl hover:scale-105 transition duration-100 ease-in-out p-3 flex flex-col items-center justify-center`}>
-            <p className="border">
-            {title}
-            
-            </p>
-            <p>
-            {text}
-            </p>
+        <div className={`w-1/6 h-96 ${classes} justify-between rounded-xl shadow-xl hover:scale-105 transition duration-100 ease-in-out p-5 flex flex-col items-center `}>
+            <div className="flex flex-col space-y-3">
+                <p className="text-2xl text-center object-top font-WorkSans italic py-1.5">
+                    {title}
+
+                </p>
+                <p className="text-base font-WorkSans">
+                    {text}
+                </p>
+            </div>
             <img src={img_src} alt="" />
-    
+            <div className=" bg-gray-100 p-2 px-4 text-lg font-WorkSans shadow-lg text-center rounded-full">
+                {button}
+            </div>
+
+
 
 
         </div>
--- components/Promiss.tsx
+++ components/Promiss.tsx
@@ -3,15 +3,21 @@ import Card from "./Card"
 const Promiss = () => {
 
     return (
-        <div className="border w-full min-h-screen flex justify-evenly p-10 items-center">
-
+        <div className="w-full border h-full p-6 bg-gray-50 ">
+<p className="border text-center font-WorkSans pt-5 pb-5 text-5xl font-normal antialiased text-gray-900"> 
+                Unsere Versprechen und Leistungen
+            </p>
+    
+        <div className="border w-full flex justify-evenly p-10 items-center">
             
-        <Card title="fotzensafft" text="" img_src="/Logos/lumix-logo_gr.svg"/>
-        <Card title="fotzensafft" text=""/>
-        <Card title="fotzensafft" text="" classes="bg-red-200"/>
-        <Card title="fotzensafft" text=""/>
-        <Card title="fotzensafft" text="hier finden sie steife schwänze"/>
+            
+        <Card title="Persönliche Beratung" classes="bg-gray-200" text="Wir bieten Ihnen eine persönlich Beratung durch unsere ExpertInnen vor Ort. Hier wird Ihre Anlage auf Ihre individuellen Bedüfnisse angepasst, denn eine Anlage kann nicht von der Stange geplant werden." button="Mehr Erfahren"/>
+        <Card title="Individuelle Anpassung" classes="bg-gray-300" text="Anlagen, zugeschnitten auf Ihre Bedürfnisse." button="Mehr Erfahren"/>
+        <Card title="Zeitnahe Montage" text="" classes="bg-yellow-200"button="Mehr Erfahren"/> 
+        <Card title="Inbetriebnahme" classes="bg-yellow-100"text=""button="Mehr Erfahren"/>
+        <Card title="Service" classes="bg-orange-100" text="" button="Mehr Erfahren"/>
         </div>
+        </div> 
     )
 
 }
