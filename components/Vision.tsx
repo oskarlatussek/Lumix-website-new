c057214db85dfa72607fd5d9926160c397b11799
SolarMax97 <maximilian.muehlhause@web.de>
2022-02-01 18:39:01 +0100

    wip wery important penis

--- components/Vision.tsx
+++ components/Vision.tsx
@@ -1,15 +1,19 @@
 const Vision = () => {
 
     return (
-        <div className="w-full border h-full min-h-screen flex flex-col p-6">
-            <p className="text-7xl mb-20">Unsere Vision</p>
-            <p className="text-2xl text-gray-800 mb-20">Unabhängige, effiziente und nachhaltige Stromerzeugung für alle. </p>
-            <div className="border w-full p-10"/>
-
-
-            
+        <div className="w-full border h-full justify-between min-h-screen flex p-6">
+            <div>
+                <p className="text-7xl mb-20">Unsere Vision</p>
+                <p className="text-2xl text-gray-800 mb-20">Unabhängige, effiziente und nachhaltige Stromerzeugung für alle. </p>
+            </div>
+            <div className="border w-full p-10">
+                <img className="" src="/Logos/photovoltaic-2138992.jpg"></img>
+            </div>
+        
 
         </div>
+        
+        
     )
 
 }
