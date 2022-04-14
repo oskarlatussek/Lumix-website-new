3bf9920948d4bbdea9011f62fa72c755a3099e7c
oskarlatussek <o.latussek@t-online.de>
2022-04-14 10:14:26 +0200

    add gtag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -13,6 +13,21 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>Lumix Solar | Solaranlagen & Batteriespeicher</title>
         <meta name="description" content="Solaranlagen und Batteriespeicher in Mannheim, der Pfalz und der Rhein-Neckar Region. Individuelle Beratung, Planung und professionelle Montage." />
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=G-J9XX7FYSJP`}
+        />
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+                            gtag('config', 'G-J9XX7FYSJP');
+                        `,
+          }}
+        />
       </Head>
       <Toolbar dark />
       <main className=''>
