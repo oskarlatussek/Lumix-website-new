a16cf3d454e258765edfb95177eccfc9b945fb02
oskarlatussek <o.latussek@t-online.de>
2022-08-03 23:45:33 +0200

    update input

--- components/section/KonfiguratorAddress.tsx
+++ components/section/KonfiguratorAddress.tsx
@@ -11,7 +11,7 @@ const KonfiguratorAddress = () => {
         <div className="bg-white/100 h-fit absolute bottom-5 rounded-xl backdrop-blur-lg flex flex-col md:flex-row items-center p-2 sm:p-5 divide-y md:divide-y-0 divide-x-0 md:divide-x divide-gray-300 mx-2">
             <p className=" text-left text-xl pr-0 md:pr-5 pb-2 sm:pb-5 md:pb-0">Jetzt <b>unverbindlich</b> Angebot für Ihr Solarprojekt <b>konfigurieren</b>.</p>
             {/* <div className="border border-black/80 w-0 h-5/6" /> */}
-            <div className="flex flex-col items-center space-y-2 sm:space-y-5 w-full pl-0 md:pl-5 pt-2 sm:pt-5 md:pt-0">
+            <div className="flex flex-col items-center md:items-stretch space-y-2 sm:space-y-5 w-full pl-0 md:pl-5 pt-2 sm:pt-5 md:pt-0">
                 <p className="text-lg w-max pl-2 -mb-0 sm:-mb-4 font-medium">Ihre Adresse</p>
 
                 <SearchLocationInput address={address} setAddress={setAddress} />
