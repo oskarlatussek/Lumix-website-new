e9f9628ce403faea9fcf1d7205d62a549bac40d3
oskarlatussek <o.latussek@t-online.de>
2023-07-30 22:24:51 +0200

    add meta pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,10 +7,30 @@ import '../styles/globals.css'
 import Script from 'next/script'
 import { getCookie } from 'cookies-next';
 import Consent from '../components/general/Conset'
+// @ts-ignore 
+import * as fbq from '../lib/fpixel'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   const consent = getCookie('localConsent');
+
+  const router = useRouter()
+
+  useEffect(() => {
+    // This pageview only triggers the first time (it's important for Pixel to have real information)
+    fbq.pageview()
+
+    const handleRouteChange = () => {
+      fbq.pageview()
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   return (
     <>
@@ -68,6 +88,24 @@ function MyApp({ Component, pageProps }: AppProps) {
           }}
         />
       )}
+
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
 
       {/* <Script
         async
