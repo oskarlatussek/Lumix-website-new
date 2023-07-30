cfffd6a183db2b179edc6026426cf685d6da590a
oskarlatussek <o.latussek@t-online.de>
2023-07-30 22:32:19 +0200

    update meta pixel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -103,6 +103,7 @@ function MyApp({ Component, pageProps }: AppProps) {
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
             fbq('init', ${fbq.FB_PIXEL_ID});
+            fbq('track', 'PageView');
           `,
         }}
       />
