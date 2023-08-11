0614f5b7d3c6f8f800a8046e32459536c0ce224f
oskarlatussek <o.latussek@t-online.de>
2023-08-11 16:54:00 +0200

    Neue Stellen

--- components/section-element/Stelle.tsx
+++ components/section-element/Stelle.tsx
@@ -1,16 +1,45 @@
+import { BsClock } from "react-icons/bs";
+import { IoIosArrowDown, IoIosArrowForward } from "react-icons/io";
+import { IoLocationOutline, IoTimeOutline } from "react-icons/io5";
+
 interface StelleProps {
     title: String;
+    type: String;
     tasks: Array<String>;
     qualifications: Array<String>;
 }
 
-const Stelle = ({ title, tasks, qualifications }: StelleProps) => {
+const Stelle = ({ title, type, tasks, qualifications }: StelleProps) => {
 
     return (
-        <div className='p-2 sm:p-5 rounded-lg border md:hover:scale-105 transition-all ease-in-out duration-300 border-yellow-400 max-w-lg min-w-fi'>
-            <p className='text-yellow-400 font-bold text-3xl w-full text-center'>{title}</p>
-            <div className='flex flex-col space-y-5 text-white p-5'>
-                <div className='p-2 md:p-5'>
+        <label className='p-2 sm:p-5 hover:cursor-pointer  select-none rounded-lg border md:hover:scale-15 transition-all ease-in-out duration-300 border-yellow-400 w-full'>
+            <input type="checkbox" className="hidden peer" />
+
+            <div className="flex justify-between items-center">
+                <div className="flex-col justify-between items-center">
+                    <p className='text-yellow-400 font-bold text-3xl w-full'>{title}</p>
+                    <div className="flex space-x-5">
+                        <div className="flex items-center space-x-2 pl-2 pt-2">
+                            <IoTimeOutline className="text-white" />
+                            <p className="text-white">{type}</p>
+                        </div>
+                        <div className="flex items-center space-x-2 pl-2 pt-2">
+                            <IoLocationOutline className="text-white" />
+                            <p className="text-white">Mannheim</p>
+                        </div>
+                    </div>
+
+                </div>
+                {/* <IoIosArrowForward className="text-yellow-400 text-3xl block peer-checked:hidden" /> */}
+                <IoIosArrowDown className="text-yellow-400 text-3xl" />
+
+                {/* <div className="flex"> */}
+
+                {/* </div> */}
+            </div>
+
+            <div className='hidden peer-checked:flex flex-col lg:flex-row space-y-5 lg:space-y-0 space-x-0 lg:space-x-5 text-white p-5'>
+                <div className='p-2 md:p-5 w-full lg:w-1/2'>
                     <p className='text-2xl pb-5'>Aufgaben</p>
                     <ul className='list-disc marker:text-yellow-400 space-y-2 text-sm md:text-base'>
                         {tasks.map((task: string) => {
@@ -18,7 +47,7 @@ const Stelle = ({ title, tasks, qualifications }: StelleProps) => {
                         })}
                     </ul>
                 </div>
-                <div className='p-2 md:p-5'>
+                <div className='p-2 md:p-5 w-full lg:w-1/2'>
                     <p className='text-2xl pb-5'>Qualifikationen</p>
                     <ul className='list-disc marker:text-yellow-400 space-y-2 text-sm md:text-base'>
                         {qualifications.map((qualification: string) => {
@@ -27,7 +56,7 @@ const Stelle = ({ title, tasks, qualifications }: StelleProps) => {
                     </ul>
                 </div>
             </div>
-        </div>
+        </label>
     )
 }
 
--- pages/karriere.tsx
+++ pages/karriere.tsx
@@ -18,12 +18,110 @@ const tasksHeizungsbauer = [
 ]
 
 const qualificationsHeizungsbauer = [
-    "Bereichsverantwortlichkeit für den neuen Geschäftszweig „Wärmepumpen“",
-    "Aufbau deines neuen Verantwortlichkeitsbereiches",
-    "Installation und Inbetriebnahme von Wärmepumpen in Privathaushalten und Gewerbegebäuden",
+    "Abgeschlossene Ausbildung als Heizungsbauer (m/w/d) oder vergleichbare Qualifikation",
+    "Erfahrung in der Installation und Wartung von Heizungsanlagen, idealerweise mit Schwerpunkt auf Wärmepumpen",
+    "Technisches Verständnis und handwerkliches Geschick",
+    "Freundliches Auftreten und sehr gute kommunikative Fähigkeiten",
+    "Motivation, Neues zu lernen und an innovativen Lösungen mitzuwirken",
+    "Selbstständige, lösungsorientierte Arbeitsweise mit Verantwortungsbewusstsein",
+    "Führerschein Klasse B"
+]
+
+const tasksElektromeister = [
+    "Bereichsverantwortlichkeit für die AC-Montagen von Photovoltaikanlagen und Ladeparks",
+    "Fachliche Führung der unterstellten Mitarbeiter",
+    "AC-seitige Montage und Inbetriebnahme von Photovoltaikanlagen, Stromspeichern, Energiemanagementsystemen und Ladeinfrastruktur",
     "Wartung und Reparatur bestehender Anlagen",
-    "Kundenberatung und - betreuung rund um das Thema Wärmepumpen",
     "Zusammenarbeit mit unserem engagierten Team, um Projekte erfolgreich umzusetzen"
+]
+
+const qualificationsElektromeister = [
+    "Du bist Elektromeister:in",
+    "Idealerweise Erfahrung im Bereich Photovoltaik",
+    "Führerschein Klasse B",
+    "Freundliches Auftreten und sehr gute kommunikative Fähigkeiten",
+    "Motivation, Neues zu lernen und an innovativen Lösungen mitzuwirken",
+    "Selbstständige, lösungsorientierte Arbeitsweise mit Verantwortungsbewusstsein"
+]
+
+const tasksSachbearbeiter = [
+    "Durchführung und Koordination von Netzanmeldungen für Photovoltaikanlagen",
+    "Erfassung und Verwaltung aller relevanten Daten und Dokumente für die Netzanmeldung",
+    "Kommunikation und Abstimmung mit dem Netzbetreiber bezüglich technischer Anforderungen und Vertragsmodalitäten",
+    "Erstellung und Prüfung von Antragsunterlagen und erforderlichen Dokumentationen",
+    "Unterstützung bei der Abwicklung von Netzanschlussverträgen und dem Einholen von Genehmigungen",
+    "Bearbeitung von Rückfragen und Anliegen seitens des Netzbetreibers oder Kund:innen",
+    "Pflege von Datenbanken und Systemen zur Dokumentation und Verfolgung von Netzanmeldungen",
+    "Mitwirkung bei der Optimierung interner Prozesse und Abläufe im Bereich der Netzanmeldungen"
+]
+
+const qualificationsSachbearbeiter = [
+    "Dein Interesse an erneuerbaren Energien und Photovoltaikanlagen ist riesig",
+    "Bestenfalls hast Du eine abgeschlossene Aus- und Weiterbildung und/oder praktische Erfahrung im kaufmännischen Bereich – andernfalls bringst Du viel bringst Motivation und Lernbereitschaft mit",
+    "Selbst bei einer Vielzahl laufender Vorgänge können wir auf Deinen Überblick über die nötigen Aufgaben und Fristen als strukturiertes Organisationstalent zählen",
+    "Du zeichnest Dich durch eine selbstständige und eigenverantwortliche Arbeitsweise aus und kannst Aufträge klar kommunizieren",
+    "Der Umgang mit gängigen MS-Office Programmen ist für Dich kein Problem"
+]
+
+const tasksAusbildung = [
+    "Du arbeitest mit jungen Menschen, die den Energiemarkt revolutionieren möchten",
+    "Du lernst, wie ein Startup funktioniert, sogar ohne Investor",
+    "Du bekommst tiefe Einblicke in alle relevanten Unternehmsabläufe",
+]
+
+const qualificationsAusbildung = [
+    "Du lebst eine Hands-on Mentalität und willst mit deiner Arbeit wirklich etwas bewegen",
+    "Du entwickelst eigene Ideen, die du aktiv mit einbringst",
+    "Selbst bei einer Vielzahl laufender Vorgänge können wir auf Deinen Überblick über die nötigen Aufgaben und Fristen als strukturiertes Organisationstalent zählen",
+    "Du zeichnest Dich durch eine selbstständige und eigenverantwortliche Arbeitsweise aus und kannst Aufträge klar kommunizieren",
+    "Der Umgang mit gängigen MS-Office Programmen ist für Dich kein Problem",
+]
+
+const tasksAssociate = [
+    "Du entscheidest zusammen mit dem Team, wo unsere Reise hingeht",
+    "Du arbeitest mit jungen Menschen, die den Energiemarkt revolutionieren möchten",
+    "Du lernst, wie ein Startup funktioniert, sogar ohne Investor",
+    "Du verantwortest Projekte, die direkt auf unsere Unternehmensziele einzahlen",
+    "Du arbeitest auf Augenhöhe mit unseren Gründern zusammen"
+]
+
+const qualificationsAssociate = [
+    "Du lebst eine Hands-on Mentalität und willst mit deiner Arbeit wirklich etwas bewegen",
+    "Du entwickelst eigene Ideen, die du aktiv mit einbringst",
+    "Selbst bei einer Vielzahl laufender Vorgänge können wir auf Deinen Überblick über die nötigen Aufgaben und Fristen als strukturiertes Organisationstalent zählen",
+    "Du zeichnest Dich durch eine selbstständige und eigenverantwortliche Arbeitsweise aus und kannst Aufträge klar kommunizieren",
+    "Der Umgang mit gängigen MS-Office Programmen ist für Dich kein Problem",
+]
+
+const tasksExpansion = [
+    "Du hilfst dabei, unser Geschäftsmodell in neuen Märkten und Regionen zu skalieren",
+    "Du identifizierst spannende Märkte und potenzielle Wachstumsregionen",
+    "Du entwickelst Strategien zum Markteintritt und planst Vertriebsaktivitäten",
+    "Du arbeitest auf Augenhöhe mit unseren Gründern zusammen"
+]
+
+const qualificationsExpansion = [
+    "Du lebst eine Hands-on Mentalität und willst mit deiner Arbeit wirklich etwas bewegen",
+    "Du entwickelst eigene Ideen, die du aktiv mit einbringst",
+    "Selbst bei einer Vielzahl laufender Vorgänge können wir auf Deinen Überblick über die nötigen Aufgaben und Fristen als strukturiertes Organisationstalent zählen",
+    "Du zeichnest Dich durch eine selbstständige und eigenverantwortliche Arbeitsweise aus und kannst Aufträge klar kommunizieren",
+    "Der Umgang mit gängigen MS-Office Programmen ist für Dich kein Problem",
+]
+
+const tasksMarketing = [
+    "Du bist Verantwortlich für unsere Marketingkampagnen und unseren Branding-Auftritt",
+    "Du optimierst bestehende Kampagenen und erstellst Neue",
+    "Du hast alle Kennzahlen im Blick und reagierst auf sich ändernde Anforderungen",
+    "Du optimierst unsere Außendarstellung bei Kunden und Bewerbern",
+    "Du erstellst Werbematerial und designst Unternehmesdokumente"
+]
+
+const qualificationsMarketing = [
+    "Du lebst eine Hands-on Mentalität und willst mit deiner Arbeit wirklich etwas bewegen",
+    "Du entwickelst eigene Ideen, die du aktiv mit einbringst",
+    "Idealerweise hast du bereits Erfahrung mit Google Ads, Social Ads und Print Ads",
+    "Du hast ein Auge für das Detail und weißt was bei unserer Zielgruppen gut ankommt",
+    "Du kannst erstklassige Visuals und Texte erstellen, die unsere Botschaft transportieren"
 ]
 
 
@@ -111,12 +209,16 @@ const Team: NextPage = () => {
                 </a> */}
                 <div className='flex flex-col items-center px-5 sm:px-10 w-full my-10' id="stellen">
                     <h3 className='text-4xl xl:text-5xl pb-5'>Offene Stellen</h3>
-                    <p className='text-white text-center max-w-2xl p-5 text-base md:text-xl pb-20'>Hier findest du unsere offenen Stellen. Bitte schick uns deine Bewerbung an <a href='mailto:bewerbung@lumix.solar' className='text-yellow-400'>bewerbung@lumix.solar</a>. Wenn aktuell keine passende Stelle für dich ausgeschrieben ist, kannst du uns auch gerne eine Initiativbewerbung schicken.</p>
+                    <p className='text-white text-center max-w-2xl p-5 text-base md:text-xl pb-20'>Hier findest du unsere offenen Stellen. Bitte schick uns deine Bewerbung an <a href='mailto:karriere@lumix.solar' className='text-yellow-400'>karriere@lumix.solar</a>. Wenn aktuell keine passende Stelle für dich ausgeschrieben ist, kannst du uns auch gerne eine Initiativbewerbung schicken.</p>
 
                     <div className='flex flex-wrap justify-center gap-5'>
-                        <Stelle title="Heizungsbauer (m/w/d) für Wärmepumpen" tasks={tasksHeizungsbauer} qualifications={qualificationsHeizungsbauer} />
-                        {/* <Stelle title="Heizungsbauer (m/w/d) für Wärmepumpen" tasks={tasksHeizungsbauer} qualifications={qualificationsHeizungsbauer} /> */}
-
+                        <Stelle title="Heizungsbauer (m/w/d) für Wärmepumpen" type="Vollzeit" tasks={tasksHeizungsbauer} qualifications={qualificationsHeizungsbauer} />
+                        <Stelle title="Elektromeister (m/w/d)" type="Vollzeit" tasks={tasksElektromeister} qualifications={qualificationsElektromeister} />
+                        <Stelle title="Solar Operations Specialist (m/w/d)" type="Vollzeit | Teilzeit" tasks={tasksSachbearbeiter} qualifications={qualificationsSachbearbeiter} />
+                        <Stelle title="Kauffrau für Büromanagement (m/w/d)" type="Ausbildung" tasks={tasksAusbildung} qualifications={qualificationsAusbildung} />
+                        <Stelle title="Founders Associate (m/w/d)" type="Praktikum | Werkstudent | Vollzeit" tasks={tasksAssociate} qualifications={qualificationsAssociate} />
+                        <Stelle title="Company Expansion (m/w/d)" type="Praktikum" tasks={tasksExpansion} qualifications={qualificationsExpansion} />
+                        <Stelle title="Brand & Performance Marketing (m/w/d)" type="Praktikum | Werkstudent | Vollzeit" tasks={tasksMarketing} qualifications={qualificationsMarketing} />
                     </div>
                 </div>
 
@@ -131,7 +233,7 @@ const Team: NextPage = () => {
 
                     <div className='flex space-x-5 items-center'>
                         <BsQuestionLg className='text-yellow-400 text-2xl flex-none' />
-                        <p className='text-white text-base'><b className='text-xl'>Kann ich mich auch initiativ bewerben?<br /> </b>Ja, wir freuen uns über Initiativbewerbungen. Sende uns deine Bewerbungsunterlagen an <a href='mailto:bewerbung@lumix.solar' className='text-yellow-400'>bewerbung@lumix.solar</a> und lass uns wissen, für welche Art von Position du dich interessierst. Wir prüfen dann, ob es passende Möglichkeiten für dich gibt.</p>
+                        <p className='text-white text-base'><b className='text-xl'>Kann ich mich auch initiativ bewerben?<br /> </b>Ja, wir freuen uns über Initiativbewerbungen. Sende uns deine Bewerbungsunterlagen an <a href='mailto:karriere@lumix.solar' className='text-yellow-400'>karriere@lumix.solar</a> und lass uns wissen, für welche Art von Position du dich interessierst. Wir prüfen dann, ob es passende Möglichkeiten für dich gibt.</p>
                     </div>
 
                     <div className='flex space-x-5 items-center'>
